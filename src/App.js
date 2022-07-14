5f402fedb7801a0777f42da7808e2e036df777ab
giank102001 <giancarlonava94@gmail.com>
2022-07-13 19:41:24 -0500

    new

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router";
-import { AnimatePresence } from "framer-motion";
 
-import firebase, { FirebaseContext } from "./firebase/index.js";
-
-import Ordenes from "./components/paginas/Ordenes.js";
-import Menu from "./components/paginas/Menu.js";
-import NuevoPlatillo from "./components/paginas/NuevoPlatillo.js";
-import Sidebar from "./components/ui/Sidebar.js";
 import Inicio from "./components/paginas/Inicio.js";
 import Admin from "./components/paginas/Admin.js";
 
