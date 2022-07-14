4000703d7ac99a16aba8ff24e41b4e5a7b7bbe6a
giank102001 <giancarlonava94@gmail.com>
2022-07-14 14:24:53 -0500

    q

--- src/components/paginas/Inicio.js
+++ src/components/paginas/Inicio.js
@@ -159,12 +159,11 @@ export default function Inicio() {
                           Shipping and taxes calculated at checkout.
                         </p>
                         <div className="mt-6">
-                          <a
-                            href="#"
+                          <p
                             className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                           >
                             Checkout
-                          </a>
+                          </p>
                         </div>
                         <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
                           <p>
