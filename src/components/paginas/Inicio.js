5c777cb191f0ccb830c066901fd0c3d0f2591990
giank102001 <giancarlonava94@gmail.com>
2022-07-13 19:48:25 -0500

    new

--- src/components/paginas/Inicio.js
+++ src/components/paginas/Inicio.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Inicio = () => {
   return (
-    <div>Inicio</div>
+    <div>
+      Inicio
+
+      <Link
+        to="/admin"
+        className="bg-blue-800 hover:bg-blue-700 inline-block mb-5 p-2 text-white uppercase font-bold"
+      >
+        Agregar Platillo
+      </Link>
+    </div>
   )
 }
 
